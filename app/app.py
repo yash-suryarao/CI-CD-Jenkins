5594a0e49b8cf73f837c537360e5f0980b71e48e
yash-suryarao <yashsuryararo224@gmail.com>
2024-09-17 16:36:52 +0000

    commiting 1

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, everyone!\nHow are you?'
+    return 'Hello, everyone!\nHow are you? Hiii...'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
